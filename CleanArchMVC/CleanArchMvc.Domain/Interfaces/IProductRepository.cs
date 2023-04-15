8d0f440afe3fb5b06296ca0c1163a1972e1a5186
Lucas Xavier <lukasx1934@gmail.com>
2023-04-15 16:23:55 -0300

    Criação das interfaces ICategoryRepository e IProductRepository

---
+++ CleanArchMVC/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetProducts();
+        public Task<Product> GetProductById(int id);
+        public Task<Product> Create();
+        public Task<Product> Update();
+        public Task<Product> Remove();
+    }
+}
